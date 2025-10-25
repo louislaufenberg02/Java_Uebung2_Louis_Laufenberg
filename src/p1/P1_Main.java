7a1743b100adfe34a0077fe75aec9ab57c74608e
louislaufenberg02 <luigispielt1@gmail.com>
2025-10-25 16:22:53 +0200

    3

--- src/p1/P1_Main.java
+++ src/p1/P1_Main.java
@@ -1,8 +1,12 @@
 package p1;
 
+import java.util.Scanner;
+
 public class P1_Main {
-	public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String zeile = s.nextLine();
+        System.out.println(zeile);
+        s.close();
     }
 }
